237a259fb6b2ae9f7c19b35ad6dfc9343673aee3
Vincent Lau <me@liuwenjun.info>
2017-04-30 22:15:32 +0800

    comment api

--- themes/next/source/js/liuwj-comments.js
+++ themes/next/source/js/liuwj-comments.js
@@ -1,47 +1,3 @@
-$(function() {
-    /* 在textarea处插入文本--Start */
-    (function($) {
-        $.fn.extend({
-            insertContent: function(myValue, t) {
-                var $t = $(this)[0];
-                if (document.selection) { // ie
-                    this.focus();
-                    var sel = document.selection.createRange();
-                    sel.text = myValue;
-                    this.focus();
-                    sel.moveStart('character', -l);
-                    var wee = sel.text.length;
-                    if (arguments.length == 2) {
-                        var l = $t.value.length;
-                        sel.moveEnd("character", wee + t);
-                        t <= 0 ? sel.moveStart("character", wee - 2 * t - myValue.length) : sel.moveStart("character", wee - t - myValue.length);
-                        sel.select();
-                    }
-                } else if ($t.selectionStart || $t.selectionStart == '0') {
-                    var startPos = $t.selectionStart;
-                    var endPos = $t.selectionEnd;
-                    var scrollTop = $t.scrollTop;
-                    $t.value = $t.value.substring(0, startPos) + myValue + $t.value.substring(endPos, $t.value.length);
-                    this.focus();
-                    $t.selectionStart = startPos + myValue.length;
-                    $t.selectionEnd = startPos + myValue.length;
-                    $t.scrollTop = scrollTop;
-                    if (arguments.length == 2) {
-                        $t.setSelectionRange(startPos - t,
-                            $t.selectionEnd + t);
-                        this.focus();
-                    }
-                } else {
-                    this.value += myValue;
-                    this.focus();
-                }
-            }
-        })
-    })(jQuery);
-    /* 在textarea处插入文本--Ending */
-});
-
-
 
 $(document).ready(function() {
 	var supportedSmilies = new Array();
@@ -87,4 +43,42 @@ $(document).ready(function() {
     });
 
     
+    function refreshComments() {
+        $.ajax({
+            url: "http://localhost/comments/?pageId=" + encodeURI(window.location.pathname),
+            type: "get",
+            dataType: "json",
+            success: function(comments) {
+                $("#ds-reset .ds-comments").empty();
+                $("#comment-template").tmpl(comments).appendTo("#ds-reset .ds-comments");
+            }
+        });
+    }
+    
+    refreshComments();
+
+    $(".ds-replybox form").submit(function() {
+        var request = {
+            pageId: window.location.pathname,
+            content: $(".ds-replybox textarea").val(),
+            fromUser: {
+                name: $(".ds-input-wrapper-name input").val(),
+                email: $(".ds-input-wrapper-email input").val(),
+                homepage: $(".ds-input-wrapper-homepage input").val(),
+            }
+        }
+
+        $.ajax({
+            url: "http://localhost/comments/",
+            type: "post",
+            contentType: "application/json",
+            data: JSON.stringify(request),
+            dataType: "json",
+            success: function(comment) {
+                $("#comment-template").tmpl(comment).appendTo("#ds-reset .ds-comments");
+            }
+        });
+
+        return false;
+    });
 });
