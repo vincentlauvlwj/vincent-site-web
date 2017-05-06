9c11f5547cde75a8362079d9122fe3e80e41ccd1
Vincent Lau <me@liuwenjun.info>
2017-05-06 19:04:07 +0800

    reply user

--- themes/next/source/js/liuwj-comments.js
+++ themes/next/source/js/liuwj-comments.js
@@ -2,19 +2,16 @@ var apiHost = "http://192.168.1.102"
 var smileyPanelHtml;
 
 function initSmileyPanelHtml() {
-    var supportedSmilies = new Array();
-    supportedSmilies.push("bowtie");
-    supportedSmilies.push("smile");
-    supportedSmilies.push("laughing");
-    supportedSmilies.push("blush");
-    supportedSmilies.push("smiley");
+    var supportedSmilies = [
+        "bowtie", "smile", "smile", "laughing", "blush", "smiley"
+    ];
 
     smileyPanelHtml = '<div id="ds-smilies-tooltip"><div class="ds-smilies-container"><ul>';
     for (var i = 0; i < supportedSmilies.length; i++) {
         var smiley = supportedSmilies[i];
         smileyPanelHtml += '<li class="emoji emoji-' + smiley + '" placeholder="[' + smiley + ']"></li>';
     }
-    smileyPanelHtml += "</ul></div></div>";
+    smileyPanelHtml += '</ul></div></div>';
 }
 
 function toggleSmileyPanel() {
@@ -50,14 +47,14 @@ function refreshLoginStatus() {
         $(".ds-input-wrapper-homepage input").val("");
     } else {
         $(".ds-toolbar").show();
-        $(".ds-toolbar .ds-visitor-name").attr("href", user.generatedHomepage);
+        $(".ds-toolbar .ds-visitor-name").attr("href", user.homepage);
         $(".ds-toolbar .ds-visitor-name").html(user.name);
         $(".ds-login-input").hide();
-        $(".ds-replybox .ds-avatar a").attr("href", user.generatedHomepage);
-        $(".ds-replybox .ds-avatar img").attr("src", user.generatedAvatar);
+        $(".ds-replybox .ds-avatar a").attr("href", user.homepage);
+        $(".ds-replybox .ds-avatar img").attr("src", user.avatar);
         $(".ds-input-wrapper-name input").val(user.name);
         $(".ds-input-wrapper-email input").val(user.email);
-        $(".ds-input-wrapper-homepage input").val(user.generatedHomepage);
+        $(".ds-input-wrapper-homepage input").val(user.homepage);
     }
 }
 
@@ -71,11 +68,25 @@ function logout() {
     refreshLoginStatus();
 }
 
-function loadComments() {
+function replyUser(userId, userName) {
+    $(".ds-reply-user").show();
+    $("#ds-reply-user-id").val(userId);
+    $("#ds-reply-user-name").html(userName);
+    $(".ds-replybox textarea").focus();
+}
+
+function cancelReply() {
+    $(".ds-reply-user").hide();
+    $("#ds-reply-user-id").val("");
+    $("#ds-reply-user-name").html("");
+}
+
+function loadComments(async) {
     $.ajax({
         url: apiHost + "/comments/?pageId=" + encodeURI(window.location.pathname),
         type: "get",
         dataType: "json",
+        async: async,
         success: function(comments) {
             if (comments.length > 0) {
                 $("#ds-comment-count").html(comments.length);
@@ -100,7 +111,11 @@ function createComment() {
         fromUser: {
             name: $.trim($(".ds-input-wrapper-name input").val()),
             email: $.trim($(".ds-input-wrapper-email input").val()),
-            homepage: $.trim($(".ds-input-wrapper-homepage input").val()),
+            homepage: $.trim($(".ds-input-wrapper-homepage input").val())
+        }, 
+        toUser: {
+            id: parseInt($("#ds-reply-user-id").val()),
+            name: $("#ds-reply-user-name").html()
         }
     }
 
@@ -109,12 +124,15 @@ function createComment() {
         $(".ds-replybox textarea").focus();
         return;
     }
+
     if (request.fromUser.name === "") {
         $(".ds-input-wrapper-name input").css("border-color", "red");
         $(".ds-input-wrapper-name input").focus();
         return;
     }
-    if (request.fromUser.email === "") {
+
+    var emailRegex = new RegExp("^[\\w-]+(\\.[\\w-]+)*@[\\w-]+(\\.[\\w-]+)+$");
+    if (request.fromUser.email === "" || !emailRegex.test(request.fromUser.email)) {
         $(".ds-input-wrapper-email input").css("border-color", "red");
         $(".ds-input-wrapper-email input").focus();
         return;
@@ -127,14 +145,13 @@ function createComment() {
         data: JSON.stringify(request),
         dataType: "json",
         success: function(comment) {
-            login(comment.fromUser);
-
             var $commentCount = $("#ds-comment-count");
             $commentCount.html(parseInt($commentCount.html()) + 1);
             $("#ds-post-placeholder").hide();
             $("#comment-template").tmpl(comment).appendTo("#ds-comments");
-
             $(".ds-replybox textarea").val("");
+            login(comment.fromUser);
+            cancelReply();
         }
     });
 }
@@ -144,9 +161,10 @@ $(document).ready(function() {
     initSmileyPanelHtml();
     refreshLoginStatus();
 
-	$(".ds-add-emote").click(toggleSmileyPanel);
+    $(".ds-add-emote").click(toggleSmileyPanel);
     $(".ds-replybox form").submit(createComment);
-    $("#ds-logout").click(logout);
+    $(".ds-logout").click(logout);
+    $(".ds-cancel-reply").click(cancelReply);
 
     $(".ds-account-control").hover(function() {
         $(this).addClass("ds-active");
@@ -154,5 +172,5 @@ $(document).ready(function() {
         $(this).removeClass("ds-active");
     });
 
-    loadComments();
+    loadComments(false);
 });
