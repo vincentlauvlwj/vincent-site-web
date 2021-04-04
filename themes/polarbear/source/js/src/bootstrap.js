4f76a5d763dfda764cc1f5df150ed5b9cfb3d724
vince <me@liuwj.me>
2021-04-04 22:51:25 +0800

    update code highlighting

--- themes/polarbear/source/js/src/bootstrap.js
+++ themes/polarbear/source/js/src/bootstrap.js
@@ -3,4 +3,12 @@ $(document).ready(function () {
     Theme.fancybox.register();
   }
   Theme.backToTop.register();
+
+  // fix code highlight
+  $(".kotlin .code .keyword").each(function() {
+    var node = $(this);
+    if (node.text() === "where" || node.text() === "set") {
+      node.removeClass("keyword");
+    }
+  });
 });
