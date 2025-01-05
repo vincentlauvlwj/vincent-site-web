c7a4dd11a20998d59f0751164309dc4736ecd763
vince <me@liuwj.me>
2025-01-05 23:27:29 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-hexo clean && hexo generate
+npx hexo clean && npx hexo generate
 
 cp -r statics/* public
 
-zip -r vincent-site-web.zip ./public
+tar -czvf public.tar.gz public
 
-scp vincent-site-web.zip root@liuwj.me:~/vincent-site-web.zip
+scp public.tar.gz ecs-user@liuwj.me:~/public.tar.gz
 
-ssh root@liuwj.me "unzip vincent-site-web.zip && rm -rf /var/www/www.liuwj.me && mv public /var/www/www.liuwj.me && rm vincent-site-web.zip"
+ssh ecs-user@liuwj.me "tar -xzvf public.tar.gz && rm -rf /var/www/www.liuwj.me && mv public /var/www/www.liuwj.me && rm public.tar.gz"
 
-rm vincent-site-web.zip
+rm public.tar.gz
